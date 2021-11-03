6e7e01fad8d2825c83886a6528ddd9db6b4aa368
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-11-03 15:56:08 +0100

    creata struttura utente base

---
+++ User/User.php
@@ -0,0 +1,39 @@
+<?php
+
+class User {
+    private $nome;
+    private $cognome;
+    private $data_nascita;
+    private $via;
+    private $premium = false;
+
+    public function __construct($_nome, $_cognome, $_data_nascita, $_via)
+    {
+        $this->nome = $_nome;
+        $this->cognome = $_cognome;
+        $this->data_nascita = $_data_nascita;
+        $this->via = $_via;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function getCognome()
+    {
+        return $this->cognome;
+    }
+
+    public function getDataNascita()
+    {
+        return $this->data_nascita;
+    }
+
+    public function getVia()
+    {
+        return $this->via;
+    }
+
+    
+}
